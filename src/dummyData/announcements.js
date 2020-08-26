16f7ed8c944685b4f01cbd2f9dd1b4b9e9f9f617
Tony Burch <TLB.dev@gmx.com>
2020-08-26 12:14:38 -0500

    dummy data added

---
+++ src/dummyData/announcements.js
@@ -0,0 +1,18 @@
+const announcements = [
+    {
+        title: "Welcome to the Dragon's Reign website.",
+        content: "Dragon's Reign is a raiding guild, that is open to all types of players at any level. If you are interested in joining please feel free to contact any dragons reign member for an invite."
+    },
+    {
+        title: "Raid: Bastion of Steel",
+        content: "Sunday | 4pm server \n\nHit: 2400+ | DPS: 900k+ \n\nDiscord: Required \n\nPlease be ready to go 10 minutes before the scheduled time so that we are able to start on time."
+    },
+    {
+        title: "Raid: Tartaric Depths Normal",
+        content: "Saturday | 6pm server \n\nHit: 2200+ | DPS: 600k+ \n\nDiscord: Recommended \n\nPlease be ready to go 10 minutes before the scheduled time so that we are able to start on time."
+    },
+    {
+        title: "Raid: Intrepid: Rise of the Phoenix",
+        content: "Friday | 6pm server \n\nHit: 2200+ | DPS: 600k+ \n\nDiscord: Recommended \n\nPlease be ready to go 10 minutes before the scheduled time so that we are able to start on time."
+    }
+]
---
+++ src/dummyData/rogueBuilds.js
@@ -0,0 +1,40 @@
+const template = {
+    role: "",
+    title: "",
+    img: "",
+    legendaries: "",
+    masteries: "",
+    buffs: "",
+    rotation: "",
+    description: "",
+    credit: "",
+    link: ""
+}
+const rogueBuilds = [
+    {
+        role: "DPS",
+        title: "BloodHound",
+        img: "",
+        legendaries: "Rapid Fire Shot | Calculated Shot | Dusk Strike | Hellfire Blades | Empyrean Bolt",
+        masteries: "Planar Replenishment | Adamant Resolve | Boundless Energy | Through The Ether | Planar Variation",
+        buffs: "Silver Tip Munitions | Rending Munition | Ranged Combat Mastery",
+        rotation: "DS-DS-DS-DS-DS-BS-CS-RFS-DS-DS-DS-DS-DS-EB-RFS",
+        description: "An easy to play build with great dps. All about using Rapid fire shot while under the effect of calculated shot, with 5 stacks from Dusk Strike, and while the enemy is afflicted by Barbed Shot.",
+        credit: "Original build and additional info in link below. Credit: Shas on rift forums",
+        link: "http://forums.riftgame.com/game-discussions/rift-guides-strategies/class-guides/rogue-guides/507591-destroyer-2-a.html"
+    }, {
+        role: "Support",
+        title: "Bardsman",
+        img: "",
+        legendaries: "Rapid Fire Shot | Calculated Shot | Cadence | Motif of Regeneration | Call: Razorbeast",
+        masteries: "Planar Replenishment | Ascended Biology | Boundless Energy | Through The Ether | Planar Variation",
+        buffs: "Fanfare of Vigor | Fanfare of Power | Ranged Combat Mastery | Rending Munitions | Electrified Munitions | Anthem of Competence or Anthem of Glory",
+        rotation: "This build has 2 rotations, one for healing and one for dps. Full details in the original post",
+        description: "A DPS/Support build that deals good damage and can privide backup heals while providing support buffs for the raid.",
+        credit: "Original build and additional info in link below. Credit: Skiye on rift forums",
+        link: "http://forums.riftgame.com/game-discussions/rift-guides-strategies/class-guides/rogue-guides/507538-4-5-bardsman-offensive-support-hybrid.html"
+    }
+
+]
+
+export default rogueBuilds
